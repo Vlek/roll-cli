fea9ba0782def7f661f2107519bbcfdbe76cd3e1
Vlek <dmm545@drexel.edu>
2020-02-10 00:43:21 -0500

    Updated the readme and also fixed the spacing on the help text.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# Project Name
+# Roll
 
+Dice roller CLI Script
+
+Makes it easy to roll dice via command line and is able handle the basic
+math functions, including parens!
+
+    Usage:
+
+        roll <nothing> -> Rolls 1d20
+
+        roll <expression> -> Rolls all dice + does math
+
+    Expressions:
+
+        1d20         - Rolls one 20-sided die
+
+        d20          - Does not require a '1' in front of 'd'
+
+        d%           - Rolls 1d100
+
+        d8 + 3d6 + 5 - Rolls 1d8, 3d6, and adds everything together
--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='roll',
     version='0.0.1',
-    packages=[''],
+    packages=['roll'],
+    install_requires=['parsley>=1.3', 'click>=7.0'],
+    entry_points={
+        'console_scripts': [
+            'roll = roll:roll'
+        ]
+    },
     url='https://github.com/vlek/roll',
     license='MIT',
     author='Derek (Vlek) McCammond',
     author_email='dmm545@drexel.edu',
-    description='CLI-based dice roller'
+    description='CLI-based dice roller',
+    keywords='dnd roll dice die rp rpg game gaming cli terminal'
 )
